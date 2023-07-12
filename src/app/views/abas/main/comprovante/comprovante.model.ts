5bb37d9e72b64ecb1f36c134659c83d9bee7471b
Arthur <arthurmartins696@gmail.com>
2023-07-12 10:05:47 -0400

    formas pagamentos

--- src/app/views/abas/main/comprovante/comprovante.model.ts
+++ src/app/views/abas/main/comprovante/comprovante.model.ts
@@ -1,5 +1,5 @@
 export class Comprovante {
-  id: number
+  id: number;
   ordem_de_servico: string;
   numero_ordem_servico: number;
   nome: string;
@@ -17,6 +17,8 @@ export class Comprovante {
   descricao_servico: string;
   preco_servico: number;
   pecas: Pecas[] = [];
+  meios_pagamento: string;
+  conta_pagamento: string;
   periodo_garantia: string;
   condicoes_garantia: string;
 }
